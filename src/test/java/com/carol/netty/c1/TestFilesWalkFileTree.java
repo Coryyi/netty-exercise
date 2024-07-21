38e4af115713626afd829b92e88668806754e326
吴嘉乐 <nianyi8410@163.com>
2024-07-21 18:22:39 +0800

    FilesWalkFileTrees删除多级目录

--- src/test/java/com/carol/netty/c1/TestFilesWalkFileTree.java
+++ src/test/java/com/carol/netty/c1/TestFilesWalkFileTree.java
@@ -1,5 +1,7 @@
 package com.carol.netty.c1;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
@@ -9,8 +11,40 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * 遍历文件目录
  */
+@Slf4j
 public class TestFilesWalkFileTree {
+    /**
+     * 删除多级目录测试
+     * @param args
+     */
     public static void main(String[] args) throws IOException {
+        //无法直接删除
+        // Files.delete(Paths.get("D:\\Netty学习\\Netty教程源码资料 - 副本"));
+        Files.walkFileTree(Paths.get("D:\\Netty学习\\Netty教程源码资料 - 副本"),new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                log.debug("进入目录==>:"+dir);
+
+
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                log.debug(file.toString());
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                log.debug("退出目录<==:"+dir);
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+    public static void main2(String[] args) throws IOException {
         AtomicInteger fileCount = new AtomicInteger();
         Files.walkFileTree(Paths.get("D:\\Netty学习\\Netty教程源码资料"),
                 new SimpleFileVisitor<Path>(){
