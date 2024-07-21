ad12b9f73cc3fb13e18cecb530a15b330b8e37ee
吴嘉乐 <nianyi8410@163.com>
2024-07-21 14:47:24 +0800

    ByteBuffer与字符串String相互转换，字符串转Bytebuffer主要三种方式，直接调用getByte()，再用buffer.put(); 或调用标准字符集encode()方法，以及调用 ByteBuffer中wrap()方法

---
+++ src/test/java/com/carol/netty/c1/TestByteBufferString.java
@@ -0,0 +1,29 @@
+package com.carol.netty.c1;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static com.carol.netty.c1.ByteBufferUtil.debugAll;
+
+public class TestByteBufferString {
+    public static void main(String[] args) {
+        // 1. 字符串转为 ByteBuffer 网络上发送数据不是直接发送字符串，是将字符串转为ByteBuffer 再将ByteBuffer写到Channel
+        // 方法一
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        buffer.put("hello".getBytes());// 默认操作系统的字符集
+        debugAll(buffer);
+
+        // 方法二 Chatset
+        //标准字符集
+        ByteBuffer buffer1 = StandardCharsets.UTF_8.encode("hello");
+        debugAll(buffer1);
+
+        // 方法三 wrap NIO提供的工具类 字节与bytebuffer中的转化
+        ByteBuffer buffer2 = ByteBuffer.wrap("hello".getBytes());
+        debugAll(buffer2);
+
+
+        System.out.println(StandardCharsets.UTF_8.decode(buffer2).toString());
+    }
+}
